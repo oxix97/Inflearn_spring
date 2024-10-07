4d97fec60cab5b81935bf738345738fd24137263
Chan <ieejo716@naver.com>
2024-10-07 16:06:07 +0900

    [CHORE] Spring 고급편

---
+++ spring_advance/src/test/java/study/spring_advance/SpringAdvanceApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring_advance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAdvanceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
