1bad7c53ab7f45a87ca3d92d5b41fb87962d5805
Chan <ieejo716@naver.com>
2024-10-08 15:00:45 +0900

    [ADD] ThreadLocal 사용으로 인한 동시성 이슈 해결

--- spring_advance/src/main/java/study/spring_advance/config/LogTraceConfig.java
+++ spring_advance/src/main/java/study/spring_advance/config/LogTraceConfig.java
@@ -2,14 +2,14 @@ package study.spring_advance.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import study.spring_advance.trace.logtrace.FieldLogTrace;
 import study.spring_advance.trace.logtrace.LogTrace;
+import study.spring_advance.trace.logtrace.ThreadLocalLogTrace;
 
 @Configuration
 public class LogTraceConfig {
 
     @Bean
     public LogTrace logTrace() {
-        return new FieldLogTrace();
+        return new ThreadLocalLogTrace();
     }
 }
