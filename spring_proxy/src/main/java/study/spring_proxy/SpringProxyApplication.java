1a22e92a0e6c3c26795b9168e2c642ad57d934ae
Chan <ieejo716@naver.com>
2024-10-11 09:40:29 +0900

    [CHORE] Spring Proxy

---
+++ spring_proxy/src/main/java/study/spring_proxy/SpringProxyApplication.java
@@ -0,0 +1,13 @@
+package study.spring_proxy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProxyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringProxyApplication.class, args);
+    }
+
+}
---
+++ spring_proxy/src/test/java/study/spring_proxy/SpringProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring_proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringProxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
