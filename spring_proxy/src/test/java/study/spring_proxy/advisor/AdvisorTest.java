580a9d241f9b7082081f584156824cee1406d9fc
Chan <ieejo716@naver.com>
2024-10-15 13:42:40 +0900

    [ADD] Pointcut 예제 코드

---
+++ spring_proxy/src/test/java/study/spring_proxy/advisor/AdvisorTest.java
@@ -0,0 +1,75 @@
+package study.spring_proxy.advisor;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.aop.ClassFilter;
+import org.springframework.aop.MethodMatcher;
+import org.springframework.aop.Pointcut;
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.support.DefaultPointcutAdvisor;
+import study.spring_proxy.common.advice.TimeAdvice;
+import study.spring_proxy.common.service.ServiceImpl;
+import study.spring_proxy.common.service.ServiceInterface;
+import study.spring_proxy.proxyfactory.ProxyFactoryTest;
+
+import java.lang.reflect.Method;
+
+@Slf4j
+public class AdvisorTest {
+
+    @Test
+    void advisorTest_V1() {
+        ServiceImpl target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(Pointcut.TRUE, new TimeAdvice());
+        proxyFactory.addAdvisor(advisor);
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+        proxy.save();
+    }
+
+    @Test
+    void advisorTest_V2() {
+        ServiceImpl target = new ServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        DefaultPointcutAdvisor advisor = new DefaultPointcutAdvisor(new MyPointcut(), new TimeAdvice());
+        proxyFactory.addAdvisor(advisor);
+        ServiceInterface proxy = (ServiceInterface) proxyFactory.getProxy();
+
+        proxy.save();
+        proxy.find();
+    }
+
+    static class MyPointcut implements Pointcut {
+        @Override
+        public ClassFilter getClassFilter() {
+            return ClassFilter.TRUE;
+        }
+
+        @Override
+        public MethodMatcher getMethodMatcher() {
+            return new MyMethodMatcher();
+        }
+    }
+
+    static class MyMethodMatcher implements MethodMatcher {
+
+        private String matchName = "save";
+
+        @Override
+        public boolean matches(Method method, Class<?> targetClass) {
+            boolean result = method.getName().equals(matchName);
+            log.info("Pointcut method = {}, targetClass = {}, result = {}", method.getName(), targetClass.getName(), result);
+            return result;
+        }
+
+        @Override
+        public boolean isRuntime() {
+            return false;
+        }
+
+        @Override
+        public boolean matches(Method method, Class<?> targetClass, Object... args) {
+            return false;
+        }
+    }
+}
