6d4b5cc84e1c380ced91881024ba047176fd2976
Chan <ieejo716@naver.com>
2024-10-22 10:53:21 +0900

    project setting

---
+++ spring_aop/src/test/java/study/spring_aop/SpringAopApplicationTests.java
@@ -0,0 +1,13 @@
+package study.spring_aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAopApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
