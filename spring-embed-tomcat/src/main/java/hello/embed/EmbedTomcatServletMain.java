69f7834fa772e94d0792e0043842ad86d2cf5b19
Chan <ieejo716@naver.com>
2024-10-25 10:04:26 +0900

    [ADD] embed tomcat 사용 예제

---
+++ spring-embed-tomcat/src/main/java/hello/embed/EmbedTomcatServletMain.java
@@ -0,0 +1,28 @@
+package hello.embed;
+
+import hello.servlet.HelloServlet;
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.connector.Connector;
+import org.apache.catalina.startup.Tomcat;
+
+import java.io.File;
+
+public class EmbedTomcatServletMain {
+    public static void main(String[] args) throws LifecycleException {
+        System.out.println("EmbedTomcatServletMain.main");
+
+        //tomcat setting
+        Tomcat tomcat = new Tomcat();
+        Connector connector = new Connector();
+        connector.setPort(8080);
+        tomcat.setConnector(connector);
+
+        //servlet setting
+        Context context = tomcat.addContext("", "/");
+        tomcat.addServlet("", "helloServlet", new HelloServlet());
+        context.addServletMappingDecoded("/hello-servlet", "helloServlet");
+
+        tomcat.start();
+    }
+}
