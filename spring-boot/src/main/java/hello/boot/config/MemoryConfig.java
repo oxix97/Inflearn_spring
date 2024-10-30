06275c7bf40f941143dfc09474d21b1f3f08e2eb
Chan <ieejo716@naver.com>
2024-10-30 09:44:16 +0900

    [ADD] ConditionalOnXXX 적용
    Conditional은 스프링에서 제공하는 것이며 이를 편리하게 사용하기 위해 ConditionalOnXXX 형식의 자동구성 클래스들을 부트에서 제공한다.

--- spring-boot/src/main/java/hello/boot/config/MemoryConfig.java
+++ spring-boot/src/main/java/hello/boot/config/MemoryConfig.java
@@ -3,12 +3,14 @@ package hello.boot.config;
 import memory.MemoryCondition;
 import memory.MemoryController;
 import memory.MemoryFinder;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@Conditional(MemoryCondition.class) // MemoryCondition 해당 되는 부분이면 Bean 등록
+//@Conditional(MemoryCondition.class) // MemoryCondition 해당 되는 부분이면 Bean 등록
+@ConditionalOnProperty(name = "memory", havingValue = "on") // MemoryCondition 직접 적용
 public class MemoryConfig {
 
     @Bean
