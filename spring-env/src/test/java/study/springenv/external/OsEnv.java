1967b9a48b712dbbc06daedfbbe2d3af46031953
Chan <ieejo716@naver.com>
2024-11-01 13:51:08 +0900

    [ADD] OS 환경변수 조회 예제

---
+++ spring-env/src/test/java/study/springenv/external/OsEnv.java
@@ -0,0 +1,15 @@
+package study.springenv.external;
+
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Map;
+
+@Slf4j
+public class OsEnv {
+    public static void main(String[] args) {
+        Map<String, String> envMap = System.getenv();
+        for (String key : envMap.keySet()) {
+            log.info("{}={}", key, envMap.get(key));
+        }
+    }
+}
