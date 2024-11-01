5c6b2fea7cd475a2597f40e4cb911057bbecfee8
Chan <ieejo716@naver.com>
2024-11-01 12:57:14 +0900

    project setting

---
+++ spring-env/src/main/java/study/springenv/SpringEnvApplication.java
@@ -0,0 +1,13 @@
+package study.springenv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEnvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringEnvApplication.class, args);
+    }
+
+}
