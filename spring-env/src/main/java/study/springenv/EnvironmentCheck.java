af0aacbe844eef76839a745c83f65f70ec5fdd77
Chan <ieejo716@naver.com>
2024-11-01 15:39:45 +0900

    [ADD] Environment 객체로 환경변수 관리 추상화
    각각의 다양한 위치에서 지정된 환경 변수를 한꺼번에 관리하며, 중복된 key값의 경우 Spring에서 지정한 우선순위를 통해 key값이 정해짐

---
+++ spring-env/src/main/java/study/springenv/EnvironmentCheck.java
@@ -0,0 +1,28 @@
+package study.springenv;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class EnvironmentCheck {
+
+    private final Environment env;
+
+    // MEMO : 서로 다른 시스템의 속성 모두 Environment 를 통해 동일한 방법으로 접근이 가능
+    // MEMO : 중복된 경우 Spring Boot 환경에서 설정된 우선 순위의 환경 변수 도출
+    @PostConstruct
+    public void init() {
+        String url = env.getProperty("url");
+        String username = env.getProperty("username");
+        String password = env.getProperty("password");
+
+        log.info("env url : {}", url);
+        log.info("env username : {}", username);
+        log.info("env password : {}", password);
+    }
+}
