088dab77fa6e955b3a3223c57b43f108fc8f2b91
Chan <ieejo716@naver.com>
2024-11-04 10:55:16 +0900

    [Refactor] PropertiesScan 적용
    @EnableConfigurationProperties를 적용하지 않아도 하위의 properties를 스캔하여 Bean으로 등록하며, 개별 설정도 가능하다.

--- external-read/src/main/java/hello/ExternalReadApplication.java
+++ external-read/src/main/java/hello/ExternalReadApplication.java
@@ -1,12 +1,16 @@
 package hello;
 
+import hello.config.MyDataSourceConfig_V1;
 import hello.config.MyDataSourceValueConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.context.annotation.Import;
 
 //@Import(MyDataSourceEnvConfig.class)
-@Import(MyDataSourceValueConfig.class)
+//@Import(MyDataSourceValueConfig.class)
+@Import(MyDataSourceConfig_V1.class)
+@ConfigurationPropertiesScan
 @SpringBootApplication(scanBasePackages = "hello.datasource")
 public class ExternalReadApplication {
 
